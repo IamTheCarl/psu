fd3f8e4681886ad6118ef9b61d05de85d9f2aa75
James Carl <jamescarl96@gmail.com>
2023-02-18 01:31:15 -0500

    Fixed config naming.

--- src/config.rs
+++ src/config.rs
@@ -8,20 +8,17 @@ use crate::power_supplies::{PowerSupply, PowerSupplyConfig};
 
 #[derive(Deserialize)]
 pub struct Config {
-    /// The default interface that should be used when a specific one isn't specified.
-    pub default_interface: String,
+    /// The default power supply that should be used when a specific one isn't specified.
+    pub default_supply: String,
     pub power_supplies: HashMap<String, PowerSupplyConfig>,
 }
 
 impl Config {
     pub async fn load() -> Result<Self> {
         let home_directory = home::home_dir().context("Could not get user's home directory.")?;
-        let config = fs::read_to_string(
-            home_directory
-                .join(".config")
-                .join("bk_precision_interface.yaml"),
-        )
-        .await?;
+        let config =
+            fs::read_to_string(home_directory.join(".config").join("bench_psu_config.yaml"))
+                .await?;
 
         let config: Self =
             serde_yaml::from_str(&config).context("Failed to deserialize config file.")?;
@@ -31,7 +28,7 @@ impl Config {
 
     pub async fn get_power_supply(&self) -> Result<Box<dyn PowerSupply>> {
         let power_supply_name =
-            std::env::var("PSU_NAME").unwrap_or_else(|_error| self.default_interface.clone());
+            std::env::var("PSU_NAME").unwrap_or_else(|_error| self.default_supply.clone());
 
         let power_supply = self
             .power_supplies
